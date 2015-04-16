23518ce3b618879070d0ae16d6133d4cecd70ee0
Thanh Man <nguyenthanhman1994@gmail.com>
2015-04-16 23:00:03 +0700

    commit

---
+++ TaiKhoan_DTO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DTO
+{
+    public class TaiKhoan_DTO
+    {
+        private int sMaTK;
+
+        public int SMaTK
+        {
+            get { return sMaTK; }
+            set { sMaTK = value; }
+        }
+        private int sMaNV;
+
+        public int SMaNV
+        {
+            get { return sMaNV; }
+            set { sMaNV = value; }
+        }
+        private string sPass;
+
+        public string SPass
+        {
+            get { return sPass; }
+            set { sPass = value; }
+        }
+    }
+}
